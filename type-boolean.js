72c7b7ee2af419f480bca2a2152aba1af6e6af6e
Peterson dos Santos <peterson.opensource@hotmail.com>
2023-02-02 22:29:14 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,9 @@
+const primieroNumero = 5;
+const segundoNumero = 5;
+
+console.log(primieroNumero === segundoNumero);
+
+const text1 =  "alura";
+const text2 = "Alura";
+
+console.log(text1 === text2);
---
+++ type-string.js
@@ -0,0 +1,17 @@
+const text1 = "Olá, mundo!";
+const text2 = 'Olá, mundo!';
+const senha = "senha-super-segura";
+const stringNumeros = "3435";
+
+const citacao = 'my name is ';
+const meuNome = "andy";
+
+//concatenacao (+)
+
+console.log(citacao + meuNome);
+
+//template string ou template literal
+
+const senha1 = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
+
