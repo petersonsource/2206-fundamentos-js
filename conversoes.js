f6d4ceb2dc2dc48dfa5b666998b55e88bca52bb3
Peterson dos Santos <peterson.opensource@hotmail.com>
2023-02-02 23:33:32 -0300

    codigo var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+//tipo de dado
+//booleanos
+
+//conversoes implicita
+const numero = 456;
+const numeroString = Number("456a");
+
+console.log(numero === numeroString)
+
+//Number()
+//String()
+console.log(numero + numeroString)
+
+//conversao explicita
+
